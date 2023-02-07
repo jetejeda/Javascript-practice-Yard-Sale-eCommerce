d3490192726bbd3937e0ebdbe9f591721afa4580
jetejeda <eduardotejedaleon@gmail.com>
2023-02-06 23:18:45 -0600

    Using the shopping cart

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const popUpUserProfile = document.querySelector('.navbar-email');
-const hamburgerMenu = document.querySelector('.menu')
+const hamburgerMenu = document.querySelector('.menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
 
 popUpUserProfile.addEventListener('click', showDesktopMenu);
 
 hamburgerMenu.addEventListener('click', showMobileMenu);
+const detail = document.querySelector('.product-detail');
+shoppingCart.addEventListener('click', function(){toggleElement(detail, document.querySelector('.mobile-menu'))});
 
 function showDesktopMenu(){
     const menu = document.querySelector('.desktop-menu');
@@ -12,9 +15,11 @@ function showDesktopMenu(){
 
 function showMobileMenu(){
     const mobileMenu  = document.querySelector('.mobile-menu')
+    detail.classList.add('inactive')
     mobileMenu.classList.toggle('inactive')
 };
 
-function toggleElement(section){//We can also pass the element from the eventListener and just have one function
+function toggleElement(section, hideElement){//We can also pass the element from the eventListener and just have one function
+    hideElement.classList.add('inactive');
     section.classList.toggle("inactive");
 }
