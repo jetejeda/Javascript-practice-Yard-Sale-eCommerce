d5a6e0af053b44224c3d252dc3fd42efb5c685b8
jetejeda <eduardotejedaleon@gmail.com>
2023-02-06 22:14:15 -0600

    Mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const popUpUserProfile = document.querySelector('.navbar-email');
+const hamburgerMenu = document.querySelector('.menu')
 
 popUpUserProfile.addEventListener('click', showDesktopMenu);
+
+hamburgerMenu.addEventListener('click', showMobileMenu);
 
 function showDesktopMenu(){
     const menu = document.querySelector('.desktop-menu');
     menu.classList.toggle('inactive')
 };
+
+function showMobileMenu(){
+    const mobileMenu  = document.querySelector('.mobile-menu')
+    mobileMenu.classList.toggle('inactive')
+};
+
+function toggleElement(section){//We can also pass the element from the eventListener and just have one function
+    section.classList.toggle("inactive");
+}
